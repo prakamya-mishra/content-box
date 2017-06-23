b38065b2eec4b67bb561c557bb54af75ba26ba6b
Prakamya_mishra <prakamya0608@gmail.com>
2017-06-23 11:53:24 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.toggle').toggleClass('active')
+  })
+
+  $('.toggle').click(function(){
+    $('.box').toggleClass('active')
+  })
+
+})
